0de1da43ea58cbf45d56818ff2ce71f605f9c87e
Jose Luis Benito Esteban <iridiobis@gmail.com>
2017-06-12 17:18:52 +0200

    Add module to include classes to help with testing

--- testingcomponents/src/main/java/es/iridiobis/testingcomponents/LiveDataExtensions.kt
+++ testingcomponents/src/main/java/es/iridiobis/testingcomponents/LiveDataExtensions.kt
@@ -1,15 +1,13 @@
-package es.iridiobis.donation
+package es.iridiobis.testingcomponents
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.Observer
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-
 
 @Throws(InterruptedException::class)
 fun <T> LiveData<T>.retrieveValue(): T {
     val data = arrayOfNulls<Any>(1)
-    val latch = CountDownLatch(1)
+    val latch = java.util.concurrent.CountDownLatch(1)
     val observer = object : Observer<T> {
         override fun onChanged(o: T?) {
             data[0] = o
@@ -18,7 +16,15 @@ fun <T> LiveData<T>.retrieveValue(): T {
         }
     }
     this.observeForever(observer)
-    latch.await(2, TimeUnit.SECONDS)
+    latch.await(2, java.util.concurrent.TimeUnit.SECONDS)
 
     return data[0] as T
+}
+
+fun <T> List<T>.asLiveData() : LiveData<List<T>> {
+    return object : MediatorLiveData<List<T>>(){
+        init {
+            value = this@asLiveData
+        }
+    }
 }
