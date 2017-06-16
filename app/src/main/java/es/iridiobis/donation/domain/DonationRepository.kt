c2ce939f8b715ae88b658d7f66dce162c35386b5
Jose Luis Benito Esteban <iridiobis@gmail.com>
2017-06-16 11:59:50 +0200

    Add the business logic to add a new donation

--- app/src/main/java/es/iridiobis/donation/domain/DonationRepository.kt
+++ app/src/main/java/es/iridiobis/donation/domain/DonationRepository.kt
@@ -3,5 +3,11 @@ package es.iridiobis.donation.domain
 import android.arch.lifecycle.LiveData
 
 interface DonationRepository {
+
     fun loadDonationsSince(date: Long): LiveData<List<Donation>>
+
+    fun loadDonationsInRange(date: Long, range: Long): LiveData<List<Donation>>
+
+    fun addDonation(donation : Donation)
+
 }
---
+++ app/src/main/java/es/iridiobis/donation/domain/NewDonationResult.kt
@@ -0,0 +1,4 @@
+package es.iridiobis.donation.domain
+
+
+class NewDonationResult(val successful : Boolean, val donations : List<Donation>)
