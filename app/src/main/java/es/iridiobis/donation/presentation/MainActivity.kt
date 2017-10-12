411e2f73d5833a6b248e65992e4f0e63846d8271
Jose Luis Benito Esteban <iridiobis@gmail.com>
2017-10-12 12:47:54 +0100

    Format next donation date

--- app/src/main/java/es/iridiobis/donation/presentation/MainActivity.kt
+++ app/src/main/java/es/iridiobis/donation/presentation/MainActivity.kt
@@ -3,29 +3,28 @@ package es.iridiobis.donation.presentation
 import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
-import es.iridiobis.donation.R
-import android.arch.lifecycle.ViewModelProviders
-import android.test.ApplicationTestCase
 import android.widget.TextView
 import es.iridiobis.donation.DonationApp
+import es.iridiobis.donation.R
 import es.iridiobis.donation.domain.Donation
-import es.iridiobis.temporizador.core.di.ComponentProvider
+import java.text.SimpleDateFormat
+import java.util.Date
 import javax.inject.Inject
 
 
 class MainActivity : LifecycleActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    @Inject lateinit var viewModelFactory : ViewModelProvider.Factory
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onBackPressed() {
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
@@ -83,8 +82,8 @@ class MainActivity : LifecycleActivity(), NavigationView.OnNavigationItemSelecte
         model.nextDonation.observe(this, Observer { nextDonation -> updateNextDonation(nextDonation!!) })
     }
 
-    private fun updateNextDonation(donation : Donation) {
+    private fun updateNextDonation(donation: Donation) {
         val nextDonationView = findViewById<TextView>(R.id.main_next_donation_date)
-        nextDonationView.text = donation.date.toString()
+        nextDonationView.text = SimpleDateFormat.getDateInstance().format(Date(donation.date))
     }
 }
